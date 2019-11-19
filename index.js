b705970042ed5d54db28ce70f14965d0d75c82a5
Oliveira <marco.oliveira@sap.com>
2019-11-19 09:40:15 -0800

    pdf support

--- index.js
+++ index.js
@@ -1,19 +1,40 @@
 'use strict'
 const createWorker = require('tesseract.js').createWorker;
 const writeIntoClipboard = require('clipboardy').writeSync;
+const pdf = require('pdf-parse');
+const fs = require('fs');
 
 const worker = createWorker({logger: m => console.log(m)});
 var language = process.argv[3] || 'por+eng';
-const recognizeFile = async () => {
+var file = process.argv[2];
+
+const recognizeImage = (file, callback)  => {
+    callback(file);
+}
+
+const recognizePDF = (file) => {
+    console.log("É UM MALDITO PDF");
+        
+    let fileBuffer = fs.readFileSync(file);
+    pdf(fileBuffer).then((data) => {
+        console.log(data);
+        writeIntoClipboard(data.text)
+    });
+}
+const recognizeFile = async (file) => {    
+    console.log(file);
+    
     await worker.load();
     await worker.loadLanguage(language);
     await worker.initialize(language);
-    const {data: {text}} = await worker.recognize(process.argv[2]);
+    const {data: {text}} = await worker.recognize(file);
     console.log(text);
     await worker.terminate();
     writeIntoClipboard(text);    
 };
 
-console.log(process.argv[2]);
-
-recognizeFile();
+if(file.substr(-4) == ".pdf"){
+    recognizePDF(file);
+}else{
+    recognizeImage(file, recognizeFile);
+}
